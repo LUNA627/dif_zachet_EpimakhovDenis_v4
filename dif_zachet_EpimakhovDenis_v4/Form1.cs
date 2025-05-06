1af28be24d7aa28db7d78c5b0337c6b49cb8c337
LUNA627 <say.samurai@yandex.ru>
2025-05-06 10:13:40 +0500

    Add files via upload

---
+++ dif_zachet_EpimakhovDenis_v4/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dif_zachet_EpimakhovDenis_v4
+{
+    public partial class Form1 : Form
+    {
+        private List<Item> Items;
+
+        public Form1()
+        {
+            InitializeComponent(); 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var letters = new List<string> { "A", "B" };
+            var nums = new List<int> { 1, 2, 3 };
+            var colors = new List<string> { "зеленый", "оранжевый" };
+
+            var ff = from l in letters
+                     from n in nums
+                     from c in colors
+                     select new Item { Bukva = l, Num = n, Color = c };
+            Items = ff.ToList();
+            GroupDisplay();
+        }
+
+        private void GroupDisplay()
+        {
+            listBox1.Items.Clear();
+
+            switch(comboBox1.SelectedIndex)
+            {
+                case 0:
+                    {
+                        var groupIsLatter = Items.GroupBy(i => i.Bukva);
+                        foreach (var group in groupIsLatter)
+                        {
+                            listBox1.Items.Add($"Группа: буква = {group.Key}");
+                            foreach(var item in group)
+                            {
+                                listBox1.Items.Add(item.ToString());
+                            }
+                        }
+                        break;
+                    }
+                case 1:
+                    {
+                        var groupIsNums = Items.GroupBy(i => i.Num);
+                        foreach (var group in groupIsNums)
+                        {
+                            listBox1.Items.Add($"Группа: цифра = {group.Key}");
+                            foreach (var item in group)
+                            {
+                                listBox1.Items.Add(item.ToString());
+                            }
+                        }
+                        break;
+                    }
+                case 2:
+                    {
+                        var groupIsColor = Items.GroupBy(i => i.Color);
+                        foreach (var group in groupIsColor)
+                        {
+                            listBox1.Items.Add($"Группа: цвет = {group.Key}");
+                            foreach (var item in group)
+                            {
+                                listBox1.Items.Add(item.ToString());
+                            }
+                        }
+                        break;
+                    }
+                default:
+                    {
+                        MessageBox.Show("Неверно формат ввода", "Ошибка");
+                        break;
+                    }
+            }
+        }
+    }
+}
---
+++ dif_zachet_EpimakhovDenis_v4/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dif_zachet_EpimakhovDenis_v4
+{
+    class Item
+    {
+        public string Bukva { get; set; }
+        public int Num { get; set; }
+        public string Color { get; set; }
+
+        public override string ToString()
+        {
+            return $"Буква - {Bukva}, Число - {Num}, Цвет - {Color}";
+        }
+
+
+    }
+}
